1efc43b06dbd58dd03acdd49cc3e29ab59319fb6
Ghavaze <ghavaze@gmail.com>
2026-07-23 13:28:29 +0200

    Update clock's flow

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -7,15 +7,19 @@ const setDate = () => {
 
     const seconds = now.getSeconds();
     const secondsDegrees = (seconds / 60) * 360 + 90;
+    secondHand.classList.toggle('hand-jump', seconds === 0);
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const minutes = now.getMinutes();
     const minutesDegrees = (minutes / 60) * 360 + 90;
+    minuteHand.classList.toggle('hand-jump', seconds === 0);
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
     const hoursDegrees = (hours / 12) * 360 + 90;
+    hourHand.classList.toggle('hand-jump', seconds === 0);
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 };
 
 setInterval(setDate, 1000);
+setDate();
