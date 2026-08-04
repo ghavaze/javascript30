353d008b907568ca1d750db44a7ec89dc7df82cd
Ghavaze <ghavaze@gmail.com>
2026-08-04 19:42:06 +0200

    Add #23 challenge and update README

---
+++ 23 - Speech Synthesis/main.js
@@ -0,0 +1,35 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+const populateVoices = (e) => {
+    voices = e.target.getVoices();
+    voicesDropdown.innerHTML = voices.map((voice) => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`).join('');
+};
+
+const setVoice = (e) => {
+    msg.voice = voices.find((voice) => voice.name === e.target.value);
+    toggle();
+};
+
+const toggle = (startOver = true) => {
+    speechSynthesis.cancel();
+    if (startOver) speechSynthesis.speak(msg);
+};
+
+const setOption = (e) => {
+    console.log(e.target.name, e.target.value);
+    msg[e.target.name] = e.target.value;
+    toggle();
+};
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach((option) => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
--- README.md
+++ README.md
@@ -42,8 +42,8 @@ This repository contains my implementation of the JavaScript30 challenge created
 |20|Speech Detection|🟩|
 |21|Geolocation|🟩|
 |22|Follow Along Link Highlighter|🟩|
-|23|Speech Synthesis|🟨|
-|24|Sticky Nav|⬜|
+|23|Speech Synthesis|🟩|
+|24|Sticky Nav|🟨|
 |25|Event Capture, Propagation, Bubbling and Once|⬜|
 |26|Stripe Follow Along Nav|⬜|
 |27|Click and Drag|⬜|
