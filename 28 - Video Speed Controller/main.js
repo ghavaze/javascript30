9702e6662b945c36aacd63a7d8c9eda2c5482bef
Ghavaze <ghavaze@gmail.com>
2026-08-07 08:31:34 +0200

    Change speed bar to click-and-drag interaction

--- 28 - Video Speed Controller/main.js
+++ 28 - Video Speed Controller/main.js
@@ -2,8 +2,10 @@ const speed = document.querySelector('.speed');
 const bar = document.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
+let isDragging = false;
 
 const handleSpeed = (e) => {
+    if (!isDragging) return;
     const y = e.pageY - e.currentTarget.offsetTop;
     const percent = y / e.currentTarget.offsetHeight;
     const min = 0.4;
@@ -15,4 +17,15 @@ const handleSpeed = (e) => {
     video.playbackRate = playbackRate;
 };
 
+const stopDragging = () => {
+    isDragging = false;
+};
+
+speed.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    handleSpeed(e);
+});
+
 speed.addEventListener('mousemove', handleSpeed);
+speed.addEventListener('mouseup', stopDragging);
+speed.addEventListener('mouseleave', stopDragging);
